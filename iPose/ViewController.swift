52511f5cf84c758693a328a8405b55a4971bb4dc
wangzhenyujiang <wangzhenyujiang@163.com>
2016-08-11 13:53:41 +0800

    [修改]：修改 Info.plist 文件。成功发送网络请求。

--- iPose/ViewController.swift
+++ iPose/ViewController.swift
@@ -11,6 +11,18 @@ import Alamofire
 import Kingfisher
 
 class ViewController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Alamofire.request(.GET, "http://nahaowan.com/api/v2/haowan/pose/list").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+    }
 }
 
